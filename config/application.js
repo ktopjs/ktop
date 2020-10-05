cf92a332ce965039f67153e832ac4ab344d291be
王森 <hdy9515@dingtalk.com>
2020-10-05 11:38:42 +0800

    FEAT: default env development

--- config/application.js
+++ config/application.js
@@ -1,4 +1,5 @@
 const path = require('path')
+process.env.NODE_ENV = process.env.NODE_ENV || 'development'
 process.env.CUSTOM_KTOP_PROJECT_PATH = path.resolve(__dirname, '../')
 const { KTopApplication } = require('@ktopjs/ktop')
 const application = new KTopApplication()
