29b4b3177b7af60c1c0b3907b395cb548a87c075
王森 <578595193@qq.com>
2020-10-05 19:10:02 +0800

    FEAT: rich migrate users demo

--- db/migrate/20200928091337_users.js
+++ db/migrate/20200928091337_users.js
@@ -1,11 +1,18 @@
+// http://knexjs.org/#Schema-createTable
 // http://knexjs.org/#Migrations
 exports.up = function(knex) {
   return knex.schema
     .createTable('users', function (table) {
-      table.increments('id')
+      table.increments('id').unsigned().primary()
       table.string('mobile', 11).notNullable()
+      // http://knexjs.org/#Schema-enum
+      table.enum('gender', ['male', 'female'])
+      table.text('decription').nullable()
       // timestamps
       table.timestamps()
+
+      table.index([ 'mobile' ])
+      // table.dropIndex([ 'mobile' ])
     })
 }
 
